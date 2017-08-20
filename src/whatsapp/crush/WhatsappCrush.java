0c83e9c47899ca58caae3f51d1110bab91fdb771
SHIVAM_AGRAWAL <shivam301296@gmail.com>
2017-08-20 10:47:03 +0530

    Everything Now

---
+++ src/whatsapp/crush/WhatsappCrush.java
@@ -0,0 +1,13 @@
+//Shivam Agrawal
+package whatsapp.crush;
+
+public class WhatsappCrush {
+
+    public static void main(String[] args) {
+    
+            WhatsappCrushGUI wcg= new WhatsappCrushGUI();
+            wcg.setVisible(true);
+    
+    }
+    
+}
